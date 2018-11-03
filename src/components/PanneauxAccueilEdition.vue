f36cf89812bdd0ab628727c4b4713ed4cb5a7966
jmarzin <jacques.marzin@free.fr>
2018-11-03 14:17:29 +0100

    Accueil fait

---
+++ src/components/PanneauxAccueilEdition.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="panneaux-accueil-edition">
+        Fonction non disponible !
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "panneauxaccueiledition",
+        props: ['donneesPanneaux'],
+        data() {
+            return {
+            }
+        },
+        methods: {
+
+        },
+        computed: {
+
+        }
+        , created() {
+            this.$emit('abandon', this.donneesContact)
+        }
+    }
+</script>
